2d682fd16f7e0cbc21c4bc6fd00fb49c65f1922e
g_brant_rosenberger <brant@room3327.com>
2018-12-19 14:23:06 -0500

    fix gateway script for yocto

--- kcf_scripts/get_dns.sh
+++ kcf_scripts/get_dns.sh
@@ -7,11 +7,10 @@ fi
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-    ifconfig $1 | grep Mask: | awk '{print $4}' | cut -d : -f 2
+    nmcli con show $1 | grep ipv4.dns: | awk '{print $2}'
 elif [[ "$unamestr" == 'Darwin' ]]; then
     echo "8.8.8.8"
 fi
 
 
 #fedora
-#    nmcli con show $1 | grep ipv4.dns: | awk '{print $2}'
--- kcf_scripts/get_gateway.sh
+++ kcf_scripts/get_gateway.sh
@@ -7,8 +7,10 @@ fi
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-    nmcli device show $1 | grep IP4.GATEWAY | awk '{print $2}'
+    ifconfig $1 | grep Mask: | awk '{print $4}' | cut -d : -f 2
 elif [[ "$unamestr" == 'Darwin' ]]; then
     echo "192.168.1.1"
 fi
 
+#for fedora
+#  nmcli device show $1 | grep IP4.GATEWAY | awk '{print $2}'
