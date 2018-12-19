0cb9ba018e6360d8cac08b7f991c3286cf792a23
g_brant_rosenberger <brant@room3327.com>
2018-12-19 15:30:58 -0500

    Update dns script

--- kcf_scripts/get_dns.sh
+++ kcf_scripts/get_dns.sh
@@ -7,10 +7,11 @@ fi
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-    nmcli con show $1 | grep ipv4.dns: | awk '{print $2}'
+    nmcli con show $1 | grep IP4.DNS | awk '{print $2}'
 elif [[ "$unamestr" == 'Darwin' ]]; then
     echo "8.8.8.8"
 fi
 
 
 #fedora
+#nmcli con show $1 | grep ipv4.dns: | awk '{print $2}'
